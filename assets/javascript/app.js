3c2ac621634f04057cdcf1f09d87767b81d01474
mccloud76 <dansolomon76@yahoo.com>
2018-11-06 17:56:10 -0700

    creates initial buttons and user created buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,10 @@
+
 var cartoonArray = ["Bugs Bunny", "Daffy Duck", "Animaniacs", "Scooby Doo"];
 
 var cartoonChar = cartoonArray[0];
 var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + cartoonChar + "&api_key=cHxD8VJ2oLDyoYjoZMvpLblY4LTo6SJF&limit=10";
+
+
 
 $.ajax({
     url: queryURL,
@@ -12,4 +15,25 @@ $.ajax({
     console.log(response.data[0].images.original_still.url)
     console.log(response.data[0].rating)
 });
+
+function addButtons() {
+    $("#button-list").empty();
+    for (var i = 0; i < cartoonArray.length; i++) {
+        var newButton = $("<button>");
+        newButton.addClass("cartoon-btn");
+        newButton.attr("data-name", cartoonArray[i]);
+        newButton.text(cartoonArray[i]);
+        $("#button-list").append(newButton);
+    }
+}
+
+$("#add-cartoon").on("click", function(event) {
+    event.preventDefault();
+    var userCartoon = $("#cartoon-input").val().trim();
+    cartoonArray.push(userCartoon);
+    addButtons();
+});
+
+
+addButtons();
 
