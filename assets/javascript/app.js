76d63af629189a78d44d5d75d0076b4d5175ecb5
mccloud76 <dansolomon76@yahoo.com>
2018-11-06 17:28:25 -0700

    basic formatting of html and js, ajax call working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,15 @@
-// api key: cHxD8VJ2oLDyoYjoZMvpLblY4LTo6SJF
+var cartoonArray = ["Bugs Bunny", "Daffy Duck", "Animaniacs", "Scooby Doo"];
+
+var cartoonChar = cartoonArray[0];
+var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + cartoonChar + "&api_key=cHxD8VJ2oLDyoYjoZMvpLblY4LTo6SJF&limit=10";
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+    console.log(response);
+    console.log(response.data[0].images.original.url);
+    console.log(response.data[0].images.original_still.url)
+    console.log(response.data[0].rating)
+});
 
